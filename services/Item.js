3201f4c520a6f963222db95ab77ddd0287fd20d9
Brendan Sacks <brendansacks21@gmail.com>
2024-06-07 14:15:48 -0400

    update app and package.json

--- services/Item.js
+++ services/Item.js
@@ -1,31 +1,30 @@
 /* class that represents an item of a grocery list.
-Item just has a name for now.
-Items get saved to a realm database.
+Item just has a name and an id.
+Items get saved to a realm database via grocery list class.
 Items can be added, deleted, and updated.
-database funcitonalities are in the database directory.
 */
 // Path: services/Item.js
 // maybe make id private
 
-import { v4 as uuidv4 } from 'uuid';
+const { v4: uuidv4 } = require('uuid');
+require('react-native-get-random-values');
 
-export default class Item {
+class Item {
   constructor(name) {
     this.id = uuidv4();
     this.name = name;
-    //eg. database.addItem(this.id, name);
   }
 
   getName() {
-    //get name from database functions
-    //eg. return database.getItemName(this.id);
     return this.name;
   }
 
   setName(name) {
     this.name = name;
-    //eg. database.setItemName(this.id, name);
   }
 }
+module.exports = Item;
 
-const item = new Item('apple');
+// const item = new Item('apple');
+// item.setName('banana');
+// console.log(item.getName()); // apple
